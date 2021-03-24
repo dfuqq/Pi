4e87bcd75f472810866e007a4b84e9825dce6c07
dfuqq <dan38449@mail.ru>
2021-03-24 23:55:57 +0500

    Компонент для режима тренировки

---
+++ src/panels/Training.js
@@ -0,0 +1,149 @@
+import React, { useState } from "react";
+import { View, Panel, PanelHeader, Button } from "@vkontakte/vkui";
+
+import { Icon24Cancel } from "@vkontakte/icons";
+
+import "./Training.css";
+
+const Training = ({ go }) => {
+    const [digit, setDigit] = useState(1);
+
+    const pi = "3.1415926535897932384626433832";
+
+    const [show, setShow] = useState(pi[0] + pi[1]);
+
+    const clicker = (e) => {
+        if (e.currentTarget.value === pi[digit + 1]) {
+            setDigit(digit + 1);
+            digit === 1 ? null : setShow(show + pi[digit]);
+        } else {
+            console.log("балдабоб");
+        }
+    };
+
+    return (
+        <View>
+            <Panel>
+                <PanelHeader
+                    left={
+                        <Icon24Cancel
+                            width={28}
+                            height={28}
+                            onClick={go}
+                            data-to='home'
+                            style={{ color: "#fff" }}
+                        />
+                    }
+                    visor={false}
+                    transparent
+                />
+                <div className='Main'>
+                    <div className='Digits'>
+                        <div className='Pi'>
+                            <div className='Off'>{show}</div>
+                            <div className='Active'>
+                                {digit === 1 ? null : pi[digit]}
+                            </div>
+                        </div>
+                    </div>
+                    <div className='Wrapper'>
+                        <div className='Keyboard'>
+                            <Button
+                                mode='overlay_outline'
+                                className='Button Button-Kbrd'
+                                onClick={clicker}
+                                value='7'
+                            >
+                                7
+                            </Button>
+                            <Button
+                                mode='overlay_outline'
+                                className='Button Button-Kbrd'
+                                onClick={clicker}
+                                value='8'
+                            >
+                                8
+                            </Button>
+                            <Button
+                                mode='overlay_outline'
+                                className='Button Button-Kbrd'
+                                onClick={clicker}
+                                value='9'
+                            >
+                                9
+                            </Button>
+                            <Button
+                                mode='overlay_outline'
+                                className='Button Button-Kbrd'
+                                onClick={clicker}
+                                value='4'
+                            >
+                                4
+                            </Button>
+                            <Button
+                                mode='overlay_outline'
+                                className='Button Button-Kbrd'
+                                onClick={clicker}
+                                value='5'
+                            >
+                                5
+                            </Button>
+                            <Button
+                                mode='overlay_outline'
+                                className='Button Button-Kbrd'
+                                onClick={clicker}
+                                value='6'
+                            >
+                                6
+                            </Button>
+                            <Button
+                                mode='overlay_outline'
+                                className='Button Button-Kbrd'
+                                onClick={clicker}
+                                value='1'
+                            >
+                                1
+                            </Button>
+                            <Button
+                                mode='overlay_outline'
+                                className='Button Button-Kbrd'
+                                onClick={clicker}
+                                value='2'
+                            >
+                                2
+                            </Button>
+                            <Button
+                                mode='overlay_outline'
+                                className='Button Button-Kbrd'
+                                onClick={clicker}
+                                value='3'
+                            >
+                                3
+                            </Button>
+                            <Button
+                                mode='overlay_outline'
+                                className='Button Button-Kbrd'
+                                style={{ visibility: "hidden" }}
+                            />
+                            <Button
+                                mode='overlay_outline'
+                                className='Button Button-Kbrd'
+                                onClick={clicker}
+                                value='0'
+                            >
+                                0
+                            </Button>
+                            <Button
+                                mode='overlay_outline'
+                                className='Button Button-Kbrd'
+                                style={{ visibility: "hidden" }}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </Panel>
+        </View>
+    );
+};
+
+export default Training;
